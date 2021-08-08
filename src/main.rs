10f11475a54de3f66d06347135a147896bee92a9
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2021-08-08 03:58:13 -0300

    refactore builder components

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,45 @@ extern crate gtk;
 
 use gdk::prelude::*;
 use gio::prelude::*;
+use glib::clone;
 use gtk::{
-    prelude::*, Builder, FileChooserAction, FileChooserDialog, MenuItem, TextBuffer, TextTagTable,
-    TextView, ToolButton, Window,
+    glib, prelude::*, Builder, FileChooserAction, FileChooserDialog, MenuItem, TextBuffer,
+    TextTagTable, TextView, ToolButton, Window,
 };
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
+
+fn builder_window(glade_src: &str, name: &str) -> Window {
+    let builder = Builder::from_string(glade_src);
+    let window: Window = builder.object(name).unwrap();
+    window
+}
+
+fn builder_text_view(glade_src: &str, name: &str) -> TextView {
+    let builder = Builder::from_string(glade_src);
+    let text_view = builder.object(name).unwrap();
+    text_view
+}
+
+fn builder_tool_button(glade_src: &str, name: &str) -> ToolButton {
+    let builder = Builder::from_string(glade_src);
+    let tool_button = builder.object(name).unwrap();
+    tool_button
+}
+
+fn builder_menu_item(glade_src: &str, name: &str) -> Option<MenuItem> {
+    let builder = Builder::from_string(glade_src);
+    let menu_item = builder.object(name);
+    menu_item
+}
+
 fn main() {
     if gtk::init().is_err() {
         println!("Failed initialize GTK.");
         return;
     }
     //Css provider custom
-    let screen = gdk::Screen::get_default().unwrap();
+    let screen = gdk::Screen::default().unwrap();
     let provider = gtk::CssProvider::new();
     gtk::CssProvider::load_from_path(&provider, "./styles.css");
     gtk::StyleContext::add_provider_for_screen(
@@ -28,56 +54,49 @@ fn main() {
     let file: Option<File> = None;
     //
     let glade_src = include_str!("mainWindow.glade");
-    let builder = Builder::from_string(glade_src);
     //
-    let window: Window = builder.get_object("mainWindow").unwrap();
+    let window = builder_window(glade_src, "mainWindow");
     window.show();
     window.connect_destroy(|_| {
         close();
     });
     //
-    let text_view: TextView = builder.get_object("text_area").unwrap();
-    let ref_to_text_view_for_button_save: TextView = text_view.clone(); //Encontrar solução para não clonar um Widget//
-    let ref_to_text_view_for_button_save_as: TextView = text_view.clone(); //Encontrar solução para não clonar um Widget//
-    let ref_to_text_view_for_button_open: TextView = text_view.clone(); //Encontrar solução para não clonar um Widget//
-    let ref_to_text_view_for_menu_open: TextView = text_view.clone(); //Encontrar solução para não clonar um Widget//
-    let ref_to_text_view_for_menu_save: TextView = text_view.clone(); //Encontrar solução para não clonar um Widget//
-
+    let text_view: TextView = builder_text_view(glade_src, "text_area");
     // text_view.show();
-    let button_new: ToolButton = builder.get_object("button_new").unwrap();
-    button_new.connect_clicked(move |_| {
-        handler_new_document(&text_view);
-    });
+    let button_new: ToolButton = builder_tool_button(glade_src, "button_new");
+    button_new.connect_clicked(clone!(@weak text_view => move |_| {
+        handler_new_document(text_view);
+    }));
     //salvar o texto escrito no TextView em disco no texto_teste.txt
-    let button_save: ToolButton = builder.get_object("button_save").unwrap();
-    button_save.connect_clicked(move |_| {
-        handler_save_file(&ref_to_text_view_for_button_save);
-    });
+    let button_save: ToolButton = builder_tool_button(glade_src, "button_save");
+    button_save.connect_clicked(clone!(@weak text_view => move |_| {
+        handler_save_file(text_view);
+    }));
     //
-    let button_save_as: ToolButton = builder.get_object("button_save_as").unwrap();
-    button_save_as.connect_clicked(move |_| handler_save_as(&ref_to_text_view_for_button_save_as));
+    let button_save_as: ToolButton = builder_tool_button(glade_src, "button_save_as");
+    button_save_as.connect_clicked(clone!(@weak text_view => move |_| handler_save_as(text_view)));
     //abre um arquivo de texto qualquer e apresenta no TextView
-    let button_open: ToolButton = builder.get_object("button_open").unwrap();
-    button_open.connect_clicked(move |_elem| {
-        handler_open_file(&ref_to_text_view_for_button_open);
-    });
+    let button_open: ToolButton = builder_tool_button(glade_src, "button_open");
+    button_open.connect_clicked(clone!(@weak text_view => move |_elem| {
+        handler_open_file(text_view);
+    }));
     //
-    let menu_quit: MenuItem = builder.get_object("menu_quit").unwrap();
+    let menu_quit: MenuItem = builder_menu_item(glade_src, "menu_quit").unwrap();
     menu_quit.connect_activate(|_| {
         close();
     });
     //
-    let menu_open: MenuItem = builder.get_object("menu_open").unwrap();
-    menu_open.connect_activate(move |_ele| {
-        handler_open_file(&ref_to_text_view_for_menu_open);
-    });
+    let menu_open: MenuItem = builder_menu_item(glade_src, "menu_open").unwrap();
+    menu_open.connect_activate(clone!(@weak text_view => move |_ele| {
+        handler_open_file(text_view);
+    }));
     //
-    let menu_save: MenuItem = builder.get_object("menu_save").unwrap();
-    menu_open.connect_activate(move |_elem| {
-        handler_save_file(&ref_to_text_view_for_menu_save);
-    });
+    let menu_save: MenuItem = builder_menu_item(glade_src, "menu_save").unwrap();
+    menu_open.connect_activate(clone!(@weak text_view => move |_elem| {
+        handler_save_file(text_view);
+    }));
     //
-    fn handler_open_file(text_view: &TextView) {
+    fn handler_open_file(text_view: TextView) {
         let action_open = FileChooserAction::Open;
         let window = Window::new(gtk::WindowType::Popup);
         let dialog_file_chooser = FileChooserDialog::with_buttons(
@@ -93,9 +112,9 @@ fn main() {
         let res = dialog_file_chooser.run();
         if res == gtk::ResponseType::Accept {
             //
-            let file = dialog_file_chooser.get_file().unwrap();
+            let file = dialog_file_chooser.file().unwrap();
             //
-            let file_path = file.get_path().unwrap();
+            let file_path = file.path().unwrap();
             //
             let mut f = File::open(file_path).unwrap();
             //
@@ -112,13 +131,13 @@ fn main() {
         dialog_file_chooser.close();
     }
     //
-    fn handler_save_file(text_view_clone: &TextView) {
+    fn handler_save_file(text_view_clone: TextView) {
         //pega o texto do text_view
-        let context = text_view_clone.get_buffer().unwrap();
-        let start = context.get_start_iter();
-        let end = context.get_end_iter();
+        let context = text_view_clone.buffer().unwrap();
+        let start = context.start_iter();
+        let end = context.end_iter();
         //
-        let text = context.get_text(&start, &end, true).unwrap();
+        let text = context.text(&start, &end, true).unwrap();
         let buf = text.as_bytes();
         //
         let mut open = OpenOptions::new();
@@ -132,7 +151,7 @@ fn main() {
         file.flush();
     }
     //
-    fn handler_save_as(text_view_clone: &TextView) {
+    fn handler_save_as(text_view_clone: TextView) {
         let action_open = FileChooserAction::Save;
         let window = Window::new(gtk::WindowType::Popup);
         let dialog_file_chooser = FileChooserDialog::with_buttons(
@@ -148,14 +167,14 @@ fn main() {
         let res = dialog_file_chooser.run();
         if res == gtk::ResponseType::Accept {
             //
-            let file_name = dialog_file_chooser.get_filename().unwrap();
+            let file_name = dialog_file_chooser.filename().unwrap();
             //
             //pega o texto do text_view
-            let context = text_view_clone.get_buffer().unwrap();
-            let start = context.get_start_iter();
-            let end = context.get_end_iter();
+            let context = text_view_clone.buffer().unwrap();
+            let start = context.start_iter();
+            let end = context.end_iter();
             //
-            let text = context.get_text(&start, &end, true).unwrap();
+            let text = context.text(&start, &end, true).unwrap();
             let buf = text.as_bytes();
             //
             let mut open = OpenOptions::new();
@@ -172,7 +191,7 @@ fn main() {
         dialog_file_chooser.close();
     }
     //
-    fn handler_new_document(text_view: &TextView) {
+    fn handler_new_document(text_view: TextView) {
         let buffer = TextBuffer::new(Some(&TextTagTable::new()));
         buffer.set_text("");
         text_view.set_buffer(Some(&buffer));
